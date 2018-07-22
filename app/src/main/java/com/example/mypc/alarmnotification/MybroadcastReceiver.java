5674a4ff2b9c3b3fa6e41c4327893add0bc8094b
AvinashKaur <avinashkaur520@gmail.com>
2018-07-22 14:27:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mypc/alarmnotification/MybroadcastReceiver.java
@@ -0,0 +1,25 @@
+package com.example.mypc.alarmnotification;
+
+/**
+ * Created by MY PC on 22-07-2018.
+ */
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+public class MybroadcastReceiver extends BroadcastReceiver
+{
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        Toast.makeText(context, "Your time is up!!!!.", Toast.LENGTH_LONG).show();
+
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+
+        vibrator.vibrate(2000);
+    }
+
+}
